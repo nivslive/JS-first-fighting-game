4118caece5c5d4aa1d20fb89162a74c7d0474b5b
Nivs <nivsoficial@gmail.com>
2023-06-09 22:43:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+    }
+
+    draw()  {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += 30
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    }
+})
+
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 400,
+    },
+    velocity: {
+        x: 3,
+        y: 3,
+    }
+})
+console.log(player)
+
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    //c.clearRect()
+    player.update()
+    enemy.update()
+}
+
+animate()
